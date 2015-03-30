583f674fb2c33130ae8875d578225d71f29bdd4b
soidid <soidid@gmail.com>
2015-03-31 00:50:36 +0800

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/** @jsx React.DOM */
+
+
+require('./normalize.css');
+require('./main.css');
+
+var React = require('react');
+var App = require('./components/App/App.jsx');
+
+React.renderComponent((
+  <App />
+), document.body);
